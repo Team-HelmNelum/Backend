a2347c4fb232285f0d7e4983769102ace6749615
woosungking <abuga991008@gmail.com>
2025-03-16 14:17:53 +0900

    env: 멀티모듈 초안 세팅

---
+++ module-app/src/test/java/com/java/moduleapp/ModuleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.moduleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
